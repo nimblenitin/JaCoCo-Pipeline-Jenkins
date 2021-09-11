2488a9c1f964f4e4dc5af05f4dee10b80984c40a
Nitin Naidu <naidunitin1@gmail.com>
2021-09-11 21:32:45 +0530

    Update ReviewAnalyserApplicationTests.java

--- src/test/java/com/nitin/ReviewAnalyser/ReviewAnalyserApplicationTests.java
+++ src/test/java/com/nitin/ReviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -2,7 +2,8 @@ package com.nitin.ReviewAnalyser;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTestclass ReviewAnalyserApplicationTests {
+@SpringBootTest
+class ReviewAnalyserApplicationTests {
     private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
     @Test public void testWordCount() {
         assertEquals(7, analyser.getWordCount("Train to win in the digital economy"));
